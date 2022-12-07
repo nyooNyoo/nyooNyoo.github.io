d25d90b9278a1a2f64b2e5c10c2bac528f073ce3
nyooNyoo <97425873+nyooNyoo@users.noreply.github.com>
2022-12-06 21:20:43 -0500

    - Edit Aesthetics

--- board.js
+++ board.js
@@ -1,27 +1,3 @@
-/*
- * The MIT License (MIT)
- *
- * Copyright (c) 2017 Marco Melilli
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 
 function CanvasBoard(matrixBoard, game) {
 	this.stage = (typeof createjs != "undefined") && new createjs.Stage("boardGame");
@@ -45,7 +21,6 @@ CanvasBoard.prototype.initBoard = function () {
 	//Draw board
 	var boardBackground = board.stage.addChild(new createjs.Shape()).set({ name: "background", x: 0, y: 0 });
 	boardBackground.graphics.beginFill("#0277BD").beginStroke("black").drawRect(60, 10, 380, 330);
-	boardBackground.graphics.beginFill("#01579B").beginStroke("black").drawRect(30, 330, 440, 20);
 
 	//Draw checkers
 	board.checkerSpaceContainer = board.stage.addChild(new createjs.Container()).set({ name: "board" });
